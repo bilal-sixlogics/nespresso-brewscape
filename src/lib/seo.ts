198725455b241500db9cacfb6c7ada1b7b0fabc2
Muhammad Qamar <muhammad.qamar@sixlogics.local>
2026-08-06 17:28:43 +0500

    update

--- src/lib/seo.ts
+++ src/lib/seo.ts
@@ -60,10 +60,9 @@ export const baseMetadata: Metadata = {
             'en-GB': `${BASE_URL}/en`,
         },
     },
-    // verification: {
-    //     // Replace with your actual verification codes when deploying
-    //     google: 'REPLACE_WITH_GOOGLE_SEARCH_CONSOLE_TOKEN',
-    // },
+    verification: {
+        google: 'cs7eUJ0hscfT6OXq6cH4MASeGmq6llEFkpYlTGeNToE',
+    },
 };
 
 /**
